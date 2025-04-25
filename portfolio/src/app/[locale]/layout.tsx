f7cd78a70d4e1a4a5fb7433fbe7ca0d53ea012f7
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2025-04-25 13:30:01 -0300

    feat: fonte sora

--- portfolio/src/app/[locale]/layout.tsx
+++ portfolio/src/app/[locale]/layout.tsx
@@ -1,19 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Sora } from "next/font/google";
 import { notFound } from 'next/navigation';
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { routing } from '@/i18n/routing';
 import "@/app/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const sora = Sora({
+  variable: "--font-sora",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Portfolio",
@@ -35,7 +32,7 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${sora.variable} antialiased`}>
         <NextIntlClientProvider>
           <div className="flex flex-col min-h-screen">
             {children}
