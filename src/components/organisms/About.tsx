2359a713bfa4815a62bcf85f4c8c37b45666449c
Vinicius Yuji Nishioka <yuu.nishioka@outlook.com>
2025-05-26 17:09:07 -0300

    feat: adicionando textos nos campos de sobre e contato

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from 'next-intl';
+import Html from '@/components/atoms/Html';
 
 export default function About() {
   const t = useTranslations('About');
@@ -6,7 +7,7 @@ export default function About() {
   return (
     <section className="flex flex-col w-full min-h-screen items-center justify-center bg-black/80 text-white">
       <h2 id="about" className="text-primary-light text-4xl scroll-mt-24 md:text-6xl lg:text-8xl">{t('title')}</h2>
-      <p className='mt-6 p-6 rounded-lg text-gray-light md:mt-12 md:bg-black/40'>texto sobre mim e piriri pororo.</p>
+      <Html text={t.raw('description')} className='flex flex-col mt-8 p-12 gap-4 rounded-lg text-gray-light md:max-w-1/3 md:mt-12 md:bg-black/40' />
     </section>
   )
 }
--- src/components/organisms/Contact.tsx
+++ src/components/organisms/Contact.tsx
@@ -1,43 +1,72 @@
-import { useTranslations } from 'next-intl';
-import { MidiaButton } from '@/components/atoms/MidiaButton';
-import GitHubIcon from '@/components/icons/GitHubIcon';
-import LinkedInIcon from '@/components/icons/LinkedInIcon';
-import EmailIcon from '@/components/icons/EmailIcon';
-import Link from 'next/link';
+import Link from "next/link";
+import { useTranslations } from "next-intl";
+import { MidiaButton } from "@/components/atoms/MidiaButton";
+import EmailIcon from "@/components/icons/EmailIcon";
+import GitHubIcon from "@/components/icons/GitHubIcon";
+import LinkedInIcon from "@/components/icons/LinkedInIcon";
 
 export default function Contact() {
-  const t = useTranslations('Contact');
+  const t = useTranslations("Contact");
+  const urls = t.raw("urls") as {
+    name: string;
+    url: string;
+  };
 
   return (
     <section className="flex flex-col w-full min-h-screen items-center justify-center bg-black/80 text-white">
-      <h2 id="contact" className="text-primary-light text-4xl scroll-mt-24 md:text-6xl lg:text-8xl">{t('title')}</h2>
-      <p className='mt-6 p-6 md:mt-6'>Sinta-se livre para me contatar.</p>
-      <div className='flex mt-6 gap-4 md:gap-8'>
-        <a
-          href="mailto:yuu.nishioka@outlook.com"
-          target='_blank'
-        >
-          <MidiaButton>
-            <EmailIcon className='w-13 h-13 m-1' />
-          </MidiaButton>
-        </a>
-        <Link
-          href="https://www.linkedin.com/in/yuji-nishioka/"
-          target='_blank'
-        >
-          <MidiaButton>
-            <LinkedInIcon className='w-10 h-10 m-3' />
-          </MidiaButton>
-        </Link>
-        <Link
-          href="https://github.com/yujinishioka"
-          target='_blank'
-        >
-          <MidiaButton>
-            <GitHubIcon className='w-16 h-16' />
-          </MidiaButton>
-        </Link>
+      <h2
+        id="contact"
+        className="text-primary-light text-4xl scroll-mt-24 md:text-6xl lg:text-8xl"
+      >
+        {t("title")}
+      </h2>
+      <p className="mt-6 p-6 md:max-w-[900px] md:mt-6 md:text-lg md:text-center">{t("description")}</p>
+      <div className="flex mt-6 gap-4 md:gap-8">
+        {Array.isArray(urls) &&
+          urls.map((item: { name: string; url: string }, index: number) => {
+            if (item.name === "E-mail") {
+              return (
+                <a
+                  key={"contact-" + index}
+                  href={`mailto:${item.url}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <MidiaButton>
+                    <EmailIcon className="w-13 h-13 m-1" />
+                  </MidiaButton>
+                </a>
+              );
+            } else if (item.name === "LinkedIn") {
+              return (
+                <Link
+                  key={"contact-" + index}
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <MidiaButton>
+                    <LinkedInIcon className="w-10 h-10 m-3" />
+                  </MidiaButton>
+                </Link>
+              );
+            } else if (item.name === "GitHub") {
+              return (
+                <Link
+                  key={"contact-" + index}
+                  href={item.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <MidiaButton>
+                    <GitHubIcon className="w-16 h-16" />
+                  </MidiaButton>
+                </Link>
+              );
+            }
+            return null;
+          })}
       </div>
     </section>
-  )
+  );
 }
